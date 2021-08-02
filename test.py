80d2cc4e6648d812aa6d37f3e01d86de950870f3
taranjit123 <88308748+taranjit123@users.noreply.github.com>
2021-08-02 14:01:46 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,17 @@
+d={'2020-1-1':4,'2020-1-2':4,'2020-1-3':6,'2020-1-4':8,'2020-1-5':2,'2020-1-6':6,'2020-1-7':2}
+print("input:",d)
+def funname(d):
+    a=1+1
+    b=2+2
+    c=3+3
+    D=4+4
+    e=5+5
+    f=6+6
+    g=7+7
+		
+    d={'mon':a,'tue':b,'wed':c,'thu':D,'fri':e,'sat':f,'sun':g}   
+   
+    print("output:",d)
+    return d
+funname(d) 
+	
